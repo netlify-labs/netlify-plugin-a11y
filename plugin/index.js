0ae5a3a9c8feb5353ac81ba3bce8aaac2cb3d26b
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-11-01 10:55:28 -0700

    Prettify files

--- plugin/index.js
+++ plugin/index.js
@@ -1,51 +1,37 @@
 const { getConfiguration } = require('./config')
-const pluginCore = require('./pluginCore');
+const pluginCore = require('./pluginCore')
 
 module.exports = {
-  async onPostBuild({
-    constants,
-    inputs,
-    utils: { build },
-  }) {
+	async onPostBuild({ constants, inputs, utils: { build } }) {
+		try {
+			const { absolutePublishDir, checkPaths, debugMode, ignoreDirectories, pa11yOpts, failWithIssues } =
+				getConfiguration({ constants, inputs })
+			const htmlFilePaths = await pluginCore.generateFilePaths({
+				absolutePublishDir,
+				ignoreDirectories,
+				fileAndDirPaths: checkPaths,
+			})
+			if (debugMode) {
+				console.log({ htmlFilePaths })
+			}
 
-    try {
-    const {
-      absolutePublishDir,
-      checkPaths,
-      debugMode,
-      ignoreDirectories,
-      pa11yOpts,
-      failWithIssues,
-    } = getConfiguration({ constants, inputs })
-    const htmlFilePaths = await pluginCore.generateFilePaths({
-      absolutePublishDir,
-      ignoreDirectories,
-      fileAndDirPaths: checkPaths,
-    });
-    if (debugMode) {
-      console.log({ htmlFilePaths });
-    }
-
-    const { report, issueCount } = await pluginCore.runPa11y({
-      build,
-      debugMode,
-      htmlFilePaths,
-      pa11yOpts,
-    });
-    if (issueCount > 0) {
-      const postRunMsg = `Pa11y found ${issueCount} accessibility violations on your site! Check the logs above for more information.`
-      console.log(report);
-      if (failWithIssues) {
-        build.failBuild(postRunMsg)
-      } else {
-        console.warn(postRunMsg)
-      }
-    }
-
-  } catch(err) {
-    build.failBuild(err.message)
-  }
-
-
-  }
+			const { report, issueCount } = await pluginCore.runPa11y({
+				build,
+				debugMode,
+				htmlFilePaths,
+				pa11yOpts,
+			})
+			if (issueCount > 0) {
+				const postRunMsg = `Pa11y found ${issueCount} accessibility violations on your site! Check the logs above for more information.`
+				console.log(report)
+				if (failWithIssues) {
+					build.failBuild(postRunMsg)
+				} else {
+					console.warn(postRunMsg)
+				}
+			}
+		} catch (err) {
+			build.failBuild(err.message)
+		}
+	},
 }
--- plugin/pluginCore.js
+++ plugin/pluginCore.js
@@ -1,67 +1,69 @@
-const pa11y = require('pa11y');
+const pa11y = require('pa11y')
 const { extname } = require('path')
 const { isDirectory, isFile } = require('path-type')
-const { results: cliReporter } = require('pa11y/lib/reporters/cli');
+const { results: cliReporter } = require('pa11y/lib/reporters/cli')
 const readdirp = require('readdirp')
 
 const EMPTY_ARRAY = []
 
 exports.runPa11y = async function ({ htmlFilePaths, pa11yOpts, build }) {
-  let issueCount = 0;
-  const results = await Promise.all(htmlFilePaths.map(async path => {
-    try {
-      const res = await pa11y(path, pa11yOpts);
-      if (res.issues && res.issues.length) {
-        issueCount += res.issues.length;
-        return cliReporter(res)
-      }
-    } catch (error) {
-      build.failBuild('pa11y failed', { error })
-    }
-  }))
+	let issueCount = 0
+	const results = await Promise.all(
+		htmlFilePaths.map(async (path) => {
+			try {
+				const res = await pa11y(path, pa11yOpts)
+				if (res.issues && res.issues.length) {
+					issueCount += res.issues.length
+					return cliReporter(res)
+				}
+			} catch (error) {
+				build.failBuild('pa11y failed', { error })
+			}
+		}),
+	)
 
-  return {
-    issueCount,
-    report: results.join(''),
-  };
-};
+	return {
+		issueCount,
+		report: results.join(''),
+	}
+}
 
-exports.generateFilePaths = async function({
-  fileAndDirPaths, // array, mix of html and directories
-  ignoreDirectories,
-  absolutePublishDir,
-  testMode,
-  debugMode
+exports.generateFilePaths = async function ({
+	fileAndDirPaths, // array, mix of html and directories
+	ignoreDirectories,
+	absolutePublishDir,
+	testMode,
+	debugMode,
 }) {
-  const excludeDirGlobs = ignoreDirectories.map(
-    // add ! and strip leading slash
-    (dir) => `!${dir.replace(/^\/+/, "")}`
-  );
-  const htmlFilePaths = await Promise.all(
-    fileAndDirPaths.map(fileAndDirPath =>
-      findHtmlFiles(`${absolutePublishDir}${fileAndDirPath}`, excludeDirGlobs)
-    )
-  )
-  return [].concat(...htmlFilePaths)
-};
+	const excludeDirGlobs = ignoreDirectories.map(
+		// add ! and strip leading slash
+		(dir) => `!${dir.replace(/^\/+/, '')}`,
+	)
+	const htmlFilePaths = await Promise.all(
+		fileAndDirPaths.map((fileAndDirPath) => findHtmlFiles(`${absolutePublishDir}${fileAndDirPath}`, excludeDirGlobs)),
+	)
+	return [].concat(...htmlFilePaths)
+}
 
 const findHtmlFiles = async function (fileAndDirPath, directoryFilter) {
-  if (await isDirectory(fileAndDirPath)) {
-    const fileInfos = await readdirp.promise(fileAndDirPath, {
-      fileFilter: '*.html',
-      directoryFilter: !!directoryFilter.length ? directoryFilter : '*'
-    })
-    return fileInfos.map(({ fullPath }) => fullPath)
-  }
+	if (await isDirectory(fileAndDirPath)) {
+		const fileInfos = await readdirp.promise(fileAndDirPath, {
+			fileFilter: '*.html',
+			directoryFilter: !!directoryFilter.length ? directoryFilter : '*',
+		})
+		return fileInfos.map(({ fullPath }) => fullPath)
+	}
 
-  if (!(await isFile(fileAndDirPath))) {
-    console.warn(`Folder ${fileAndDirPath} was provided in "checkPaths", but does not exist - it either indicates something went wrong with your build, or you can simply delete this folder from your "checkPaths" in netlify.toml`)
-    return EMPTY_ARRAY
-  }
+	if (!(await isFile(fileAndDirPath))) {
+		console.warn(
+			`Folder ${fileAndDirPath} was provided in "checkPaths", but does not exist - it either indicates something went wrong with your build, or you can simply delete this folder from your "checkPaths" in netlify.toml`,
+		)
+		return EMPTY_ARRAY
+	}
 
-  if (extname(fileAndDirPath) !== '.html') {
-    return EMPTY_ARRAY
-  }
+	if (extname(fileAndDirPath) !== '.html') {
+		return EMPTY_ARRAY
+	}
 
-  return [fileAndDirPath]
+	return [fileAndDirPath]
 }
