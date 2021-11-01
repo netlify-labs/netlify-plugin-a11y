ed28d33081e219f70acd92a3f44dd597df31cd23
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-11-01 12:24:33 -0700

    Remove testrun file

--- testrun.js
+++
@@ -1,7 +0,0 @@
-const pa11y = require('pa11y');
-const path = require('path');
-const file = path.join(__dirname, '/demo/index.html');
-(async function() {
-  const res = await pa11y(file);
-  console.log({ res: res.issues });
-})();
