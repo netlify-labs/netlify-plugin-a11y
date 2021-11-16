5fbb01272aedf52ac82b7d2682ab1c6f2218fd24
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-11-15 21:43:46 -0800

    Avoid passing whole report to failBuild utility
    Removes generatePostrunMessage function

--- src/index.js
+++ src/index.js
@@ -22,26 +22,19 @@ module.exports = {
 				htmlFilePaths,
 				pa11yOpts,
 			})
-			const log = issueCount === 0 ? console.log : failWithIssues ? build.failBuild : console.warn
-			log(generatePostrunMessage(issueCount, report))
+			const reportSummary =
+				`${issueCount === 0 ? 'No' : issueCount} accessibility issues found!` +
+				(issueCount > 0 ? ' Check the logs for more information.' : '')
+
+			console.log(report)
+
+			if (failWithIssues) {
+				build.failBuild(reportSummary)
+			} else {
+				console.warn(pico.magenta(reportSummary))
+			}
 		} catch (err) {
 			build.failBuild(err.message)
 		}
 	},
-}
-
-/**
- * Generates the message sent to the build log after a11y checks have been peformed.
- * @param {number} issueCount
- * @param {string} report
- */
-function generatePostrunMessage(issueCount, report) {
-	// Only print a line between the report and the summary
-	// if the report has any contents.
-	const spacer = report.length > 0 ? '\n' : ''
-	const humanReadableCount = issueCount === 0 ? 'No' : issueCount
-	const summary = `${humanReadableCount} accessibility violations found! ${
-		issueCount > 0 ? 'Check the logs above for more information.' : ''
-	}`
-	return report + spacer + pico.magenta(summary)
 }
