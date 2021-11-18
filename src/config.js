0b45f4ca0aedb2ada28dae98f55b57998d3dcc71
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-11-17 18:15:13 -0800

    Use persistent Puppeteer instance (#63)
    * Install puppeteer
    
    * Export puppeteer instance from config
    
    Also make config async
    
    * Create separate getPa11yOpts function
    
    * Handle browser instance in pluginCore

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 // @ts-check
+const puppeteer = require('puppeteer')
 
 const DEFAULT_CHECK_PATHS = ['/']
 const DEFAULT_FAIL_WITH_ISSUES = true
@@ -17,14 +18,27 @@ const getConfiguration = ({
 		checkPaths: checkPaths || DEFAULT_CHECK_PATHS,
 		ignoreDirectories: ignoreDirectories || DEFAULT_IGNORE_DIRECTORIES,
 		failWithIssues: failWithIssues !== undefined ? failWithIssues : DEFAULT_FAIL_WITH_ISSUES,
-		pa11yOpts: {
-			runners: PA11Y_RUNNERS,
-			userAgent: PA11Y_USER_AGENT,
-			standard: wcagLevel || PA11Y_DEFAULT_WCAG_LEVEL,
-		},
+		wcagLevel: wcagLevel || PA11Y_DEFAULT_WCAG_LEVEL,
+	}
+}
+
+const getPa11yOpts = async (wcagLevel) => {
+	const browser = await puppeteer.launch({
+		// NB: Allows iframes to load when HTML page is visited from filesystem.
+		// Without this, a page with an iframe will crash pa11y
+		args: ['--disable-web-security'],
+		ignoreHTTPSErrors: true,
+	})
+
+	return {
+		browser,
+		runners: PA11Y_RUNNERS,
+		userAgent: PA11Y_USER_AGENT,
+		standard: wcagLevel || PA11Y_DEFAULT_WCAG_LEVEL,
 	}
 }
 
 module.exports = {
 	getConfiguration,
+	getPa11yOpts,
 }
