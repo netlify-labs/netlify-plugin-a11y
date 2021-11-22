40fc6db1a64707c31817605d042362ac0b744c0b
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-11-22 13:51:12 -0800

    Clean up server file

--- src/server.js
+++ src/server.js
@@ -7,19 +7,13 @@ const SERVER_PORT = '9000'
 const SERVER_PATH = 'localhost:' + SERVER_PORT
 
 const server = http.createServer(async function (req, res) {
-	res.setHeader('Content-type', 'text/html')
-
 	try {
-		const data = await fs.readFile('foo')
-		return res.end(data)
+		res.writeHead(200, { 'Content-type': 'text/html' })
+		res.end(await fs.readFile(req.url))
 	} catch (err) {
 		res.statusCode = 500
 		res.end(`Error getting the file: ${err}.`)
 	}
-})
-
-server.addListener('listening', function () {
-	console.log('Listening for connections on port ' + this.address().port)
 })
 
 module.exports = {
